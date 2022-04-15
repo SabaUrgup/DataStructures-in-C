fd1604a8c1a0c2f1d555cd1b6c79a54247fd6bbc
Saba ÜRGÜP <76062971+SabaUrgup@users.noreply.github.com>
2022-04-15 17:52:32 +0300

    create node

---
+++ BLM212-cse-btu/exp4/exp4.h
@@ -0,0 +1,24 @@
+typedef struct node {
+    void* dataPtr;
+    struct node* link;
+} NODE;
+/* =================== createNode ====================
+Creates a node in dynamic memory and stores data
+pointer in it.
+ Pre itemPtr is pointer to data to be stored.
+ Post node created and its address returned.
+*/
+NODE* createNode (void* itemPtr) {
+    NODE* nodePtr;
+    nodePtr = (NODE*) malloc (sizeof (NODE));
+    nodePtr->dataPtr = itemPtr;
+    nodePtr->link = NULL;
+    return nodePtr;
+}
+
+void* larger (void* dataPtr1, void* dataPtr2, int (*ptrToCmpFun)(void*, void*)) {
+    if ((*ptrToCmpFun) (dataPtr1, dataPtr2) > 0)
+        return dataPtr1;
+    else
+        return dataPtr2;
+} // larger
